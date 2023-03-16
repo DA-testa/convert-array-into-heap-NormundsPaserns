b05c91747971fd99e48889dae16e48955a055f99
Normunds Paserns <124579224+NormundsPaserns@users.noreply.github.com>
2023-03-16 10:46:05 +0000

    commit

--- build_heap.py
+++ build_heap.py
@@ -42,7 +42,6 @@ def main():
         data = list(map(int, input().split()))
     except EOFError:
         print("Error: no input data type available.")
-        sys.exit(1)
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
